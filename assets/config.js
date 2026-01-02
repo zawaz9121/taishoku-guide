bdf1f4f073318808b90c19ef05095e695b136ceb
zawaz9121 <zawaz9121@gmail.com>
2026-01-02 21:14:20 +0900

    Create config.js

---
+++ assets/config.js
@@ -0,0 +1,31 @@
+window.AFF = {
+  // C：みやび（問い合わせ/交渉）
+  C: {
+    name: "弁護士法人みやび（問い合わせ・交渉）",
+    type: "弁護士（交渉/不安が強い人向け）",
+    url: "https://af.moshimo.com/af/c/click?a_id=5325612&p_id=2002&pc_id=4047&pl_id=27336",
+    imp: "https://i.moshimo.com/af/i/impression?a_id=5325612&p_id=2002&pc_id=4047&pl_id=27336"
+  },
+
+  // A：ガイア（成約）
+  A: {
+    name: "弁護士法人ガイア法律事務所",
+    type: "弁護士（安心重視）",
+    url: "https://af.moshimo.com/af/c/click?a_id=5325613&p_id=5546&pc_id=15198&pl_id=71549",
+    imp: "https://i.moshimo.com/af/i/impression?a_id=5325613&p_id=5546&pc_id=15198&pl_id=71549",
+    banner: "https://image.moshimo.com/af-img/5231/000000071549.jpg",
+    bannerW: 320,
+    bannerH: 50
+  },
+
+  // B：ガーディアン（労組）
+  B: {
+    name: "退職代行ガーディアン",
+    type: "労組（バランス重視）",
+    url: "https://af.moshimo.com/af/c/click?a_id=5325614&p_id=3214&pc_id=7541&pl_id=85674",
+    imp: "https://i.moshimo.com/af/i/impression?a_id=5325614&p_id=3214&pc_id=7541&pl_id=85674",
+    banner: "https://image.moshimo.com/af-img/2076/000000085674.jpg",
+    bannerW: 300,
+    bannerH: 250
+  }
+};
